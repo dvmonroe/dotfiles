d2b4d444102ff76aa6bc885fdce70d31142e5123
Drew Monroe <dvmonroe6@gmail.com>
2016-05-13 15:29:54 -0500

    first commit ~ includes brew.sh

---
+++ brew.sh
@@ -0,0 +1,51 @@
+brew update
+
+brew upgrade --all
+brew install autoconf
+brew install automake
+brew install bash-completion
+brew install brew-cask
+brew install docker
+brew install docker-compose
+brew install docker-machine
+brew install elasticsearch
+brew install freetype
+brew install git
+brew install gnupg
+brew install graphviz
+brew install heroku-toolbelt
+brew install imagemagick --with-webp
+
+brew install jpeg
+brew install libgpg-error
+brew install libksba
+brew install libpng
+brew install libtiff
+brew install libtool
+brew install libyaml
+brew install lzo
+brew install ngrok
+brew install node
+brew install openssl
+brew install pkg-config
+brew install posgresql
+brew install readline
+brew install redis
+brew install wget
+brew install xz
+
+
+# https://github.com/Homebrew/homebrew-services
+brew tap homebrew/services
+
+# kept up with homebrew
+# https://github.com/caskroom/homebrew-cask
+brew cask install google-chrome
+brew cask install evernote
+brew cask install postman
+brew cask install slack
+brew cask install spotify
+brew cask install sublime-text3
+brew cask install tor-browser
+
+brew cleanup

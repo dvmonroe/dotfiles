ad4bd55cc4b5e6eb8c101b72712138f494e45ecd
Andrew Mason <andrew.mason@n2pub.com>
2018-10-04 04:22:13 -0400

    remove duplicate and fix typo

--- brew.sh
+++ brew.sh
@@ -75,7 +75,6 @@ brew cask install adobe-reader
 brew cask install ethereum-wallet
 brew cask install google-chrome
 brew cask install golem
-brew cask install google-chrome
 brew cask install iterm2
 brew cask install java
 brew cask install kitematic
@@ -86,7 +85,7 @@ brew cask install owasp-zap
 brew cask install postman
 brew cask install slack
 brew cask install spotify
-brew cask install torbrowser
+brew cask install tor-browser
 brew cask install vagrant
 brew cask install virtualbox
 
